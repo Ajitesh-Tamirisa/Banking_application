4e1b0749b0195bcaf12f0e0453a41a8d06d199f8
Ajitesh-Tamirisa <ajiteshsai@gmail.com>
2024-07-29 22:37:30 -0700

    CI workflow env variables fix

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -10,26 +10,26 @@ import (
 	_ "github.com/lib/pq"
 )
 
+// Load .env file in non-CI environments
+func init() {
+	if os.Getenv("GITHUB_ACTIONS") != "true" {
+		if err := godotenv.Load(); err != nil {
+			log.Println("Error loading .env file")
+		}
+	}
+}
+
+var (
+	dbDriver = os.Getenv("DB_DRIVER")
+	dbSource = os.Getenv("DB_SOURCE")
+)
+
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	// Load environment variables from the .env file
-	env_err := godotenv.Load()
-	if env_err != nil {
-		log.Fatalf("Error loading .env file: %v", env_err)
-	}
-
-	// Fetch driver and source from environment variables
-	dbDriver := os.Getenv("DB_DRIVER")
-	dbSource := os.Getenv("DB_SOURCE")
-
-	// Check if the environment variables were fetched successfully
-	if dbDriver == "" {
-		log.Fatal("DB_DRIVER not set in environment variables")
-	}
-	if dbSource == "" {
-		log.Fatal("DB_SOURCE not set in environment variables")
+	if dbDriver == "" || dbSource == "" {
+		log.Fatal("Database environment variables not set")
 	}
 
 	var err error
@@ -41,3 +41,47 @@ func TestMain(m *testing.M) {
 
 	os.Exit(m.Run())
 }
+
+// package db
+
+// import (
+// 	"database/sql"
+// 	"log"
+// 	"os"
+// 	"testing"
+
+// 	"github.com/joho/godotenv"
+// 	_ "github.com/lib/pq"
+// )
+
+// var testQueries *Queries
+// var testDB *sql.DB
+
+// func TestMain(m *testing.M) {
+// 	// Load environment variables from the .env file
+// 	env_err := godotenv.Load()
+// 	if env_err != nil {
+// 		log.Fatalf("Error loading .env file: %v", env_err)
+// 	}
+
+// 	// Fetch driver and source from environment variables
+// 	dbDriver := os.Getenv("DB_DRIVER")
+// 	dbSource := os.Getenv("DB_SOURCE")
+
+// 	// Check if the environment variables were fetched successfully
+// 	if dbDriver == "" {
+// 		log.Fatal("DB_DRIVER not set in environment variables")
+// 	}
+// 	if dbSource == "" {
+// 		log.Fatal("DB_SOURCE not set in environment variables")
+// 	}
+
+// 	var err error
+// 	testDB, err = sql.Open(dbDriver, dbSource)
+// 	if err != nil {
+// 		log.Fatal("Cannot connect to Database:", err)
+// 	}
+// 	testQueries = New(testDB)
+
+// 	os.Exit(m.Run())
+// }
