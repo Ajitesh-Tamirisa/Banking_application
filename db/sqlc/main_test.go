e5f0fd83d528439cf8cfbe31c458b8612bc56747
Ajitesh-Tamirisa <ajiteshsai@gmail.com>
2024-08-01 10:51:51 -0700

    Env undo

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,32 +6,18 @@ import (
 	"os"
 	"testing"
 
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
-// Load .env file in non-CI environments
-func init() {
-	if os.Getenv("GITHUB_ACTIONS") != "true" {
-		if err := godotenv.Load(); err != nil {
-			log.Println("Error loading .env file")
-		}
-	}
-}
-
-var (
-	dbDriver = os.Getenv("DB_DRIVER")
-	dbSource = os.Getenv("DB_SOURCE")
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:password@localhost:5432/banking_app?sslmode=disable"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	if dbDriver == "" || dbSource == "" {
-		log.Fatal("Database environment variables not set")
-	}
-
 	var err error
 	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
